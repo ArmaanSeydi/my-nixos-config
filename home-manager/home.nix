49ea2fc4f243f8ffb22d8141bb93921d4971a8c5
Armaan Seydi <armaanseydi@gmail.com>
2024-03-30 20:57:05 +0330

    allowing unfree pkgs

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -9,6 +9,9 @@
   # Home-manager version. DO NOT CHANGE THIS!
   home.stateVersion = "23.11";
 
+  # Allowing unfree pkgs
+  nixpkgs.config.allowUnfree = true;
+
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
